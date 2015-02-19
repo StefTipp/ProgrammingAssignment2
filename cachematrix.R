f7d0fde3ecaf18a98afeb05694ef10ec3a5e72fe
StefTipp <tippmann@chalmers.se>
2015-02-19 21:31:54 +0100

    Update cachematrix.R
    The updated version of cachematrix.R contains the two functions makeCacheMatrix and cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -2,8 +2,8 @@
 ## set the value of the matrix, get the value of the matrix, set the inverse of the matrix and 
 ## get the inverse of the matrix
 
-## makeCacheMatrix receives an input matrix x, which will then be saved in the cache; the output list contains the four 
-## different functions
+## makeCacheMatrix receives an input matrix x, which will then be saved in the cache; the output list contains four 
+## different functions which will be called when cacheSolve is running
 
 makeCacheMatrix <- function(x = matrix()) {
 
@@ -14,7 +14,7 @@ makeCacheMatrix <- function(x = matrix()) {
     m<<-NULL  # sets the inverse to NULL (provides default)
   }
   get<-function() x  # returns the matrix x
-  setmatrix<-function(solve) m<<- solve # saves the inverse to m in the enclosing environment / caches it
+  setmatrix<-function(inv) m<<- inv # saves the inverse to m in the enclosing environment / caches it; m is NULL the first time
   getmatrix<-function() m  # returns the inverse of the matrix; m is NULL the first time this is called
   list(set=set, get=get,  # creates a list containing all the four functions set, get, setmatrix and getmatrix 
        setmatrix=setmatrix,
@@ -23,8 +23,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 
-## cacheSolve retrieves the inverse of the input matrix x from the cache if has been calculated already,
-## otherwise it will do so
+## cacheSolve checks if the inverse of the input matrix x has been calculated already, if not it will do so
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
